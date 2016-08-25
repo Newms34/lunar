f827af04e92718e626a006d62ac563bb08d53ed1
newms34 <newms3450@gmail.com>
2016-08-25 19:45:09 -0400

    most of game works now. tweakin controls a bit. Still need to consider phone support (multi phones?)

--- build/js/controllers/MainController.js
+++ build/js/controllers/MainController.js
@@ -5,7 +5,7 @@ app.controller("MainController", function($scope, $window) {
     //set an initial username
     $scope.mobilecheck = function() {
         $scope.grav = 99.546;
-        $scope.rcsThrust = 1.815131578947368/10;
+        $scope.rcsThrust = 1.815131578947368 / 10;
         var check = false;
         (function(a) {
             if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
@@ -34,6 +34,8 @@ app.controller("MainController", function($scope, $window) {
         rcsNoz, //for easy reference later to shut them all OFF.
         ready = false; //just a boolean to let us know when all the polys are drawn!
     $scope.throttle = 50; //max 100, min 0.
+    $scope.thirdPerson = true; //if this is true, the ship moves. if its not, the MOON moves!
+    $scope.activeGame = false;
     $scope.rot = {
         x: 0, //pitch
         y: 0, //yaw
@@ -47,7 +49,7 @@ app.controller("MainController", function($scope, $window) {
     $scope.tran = {
         x: 0, //left-right
         y: 0, //up-down
-        z: 300 //back-front
+        z: -300 //back-front
     };
     $scope.tranV = {
         x: 0, //vel left-right
@@ -199,14 +201,15 @@ app.controller("MainController", function($scope, $window) {
     buildIt();
     window.onkeyup = function(e) {
         moveMe('off');
-        $scope.tranAV.x =0;
+        $scope.tranAV.x = 0;
         $scope.tranAV.y = 0;
         $scope.tranAV.z = 0;
     }
     window.onkeydown = function(e) {
         //movement!
-        //zero the rcs accel
-        $scope.tranAV.x =0;
+        console.log(e.which)
+            //zero the rcs accel
+        $scope.tranAV.x = 0;
         $scope.tranAV.y = 0;
         $scope.tranAV.z = 0;
         switch (e.which) {
@@ -263,6 +266,16 @@ app.controller("MainController", function($scope, $window) {
             case 32:
                 e.preventDefault();
                 $scope.throttle = 0;
+                break;
+            case 71:
+                e.preventDefault();
+                $scope.activeGame = !$scope.activeGame;
+                if ($scope.activeGame) {
+                    $scope.tranV.y = 0;
+                }
+                break;
+            case 80:
+                $scope.thirdPerson = !$scope.thirdPerson;
                 break;
         }
     }
@@ -336,22 +349,22 @@ app.controller("MainController", function($scope, $window) {
                 $scope.dRot.z += $scope.rotPwr;
                 break;
             case 'translate-up':
-                $scope.tranAV.y = $scope.rcsThrust*-4;
+                $scope.tranAV.y = $scope.rcsThrust * -4;
                 break;
             case 'translate-down':
-                $scope.tranAV.y = $scope.rcsThrust*4;
+                $scope.tranAV.y = $scope.rcsThrust * 4;
                 break;
             case 'translate-left':
-                $scope.tranAV.x = $scope.rcsThrust*-4;
+                $scope.tranAV.x = $scope.rcsThrust * -4;
                 break;
             case 'translate-right':
-                $scope.tranAV.x = $scope.rcsThrust*4;
+                $scope.tranAV.x = $scope.rcsThrust * 4;
                 break;
             case 'translate-forward':
-                $scope.tranAV.z = $scope.rcsThrust*-4;
+                $scope.tranAV.z = $scope.rcsThrust * -4;
                 break;
             case 'translate-back':
-                $scope.tranAV.z = $scope.rcsThrust*4;
+                $scope.tranAV.z = $scope.rcsThrust * 4;
                 break;
         }
     }
@@ -361,25 +374,61 @@ app.controller("MainController", function($scope, $window) {
         $scope.rot.z += $scope.dRot.z;
         //use rot.x, rot.z to get change in x,y,z dirs
         var xyzPercs = fromAngles($scope.rot.x, $scope.rot.z);
-        console.log(xyzPercs.x, xyzPercs.y, xyzPercs.z)
-            //note that somehow x and y axes are SWITCHED!
-        $scope.tranA.x = (xyzPercs.y * ($scope.throttle / 100) * 184.16 / 80)+$scope.tranAV.x;
-        $scope.tranA.y = ((xyzPercs.x * ($scope.throttle / 100) * -184.16 / 80) + $scope.grav / 80)+$scope.tranAV.y;
-        $scope.tranA.z = (xyzPercs.z * ($scope.throttle / 100) * -184.16 / 80)+$scope.tranAV.z;
-        console.log('accel y', $scope.tranA.y)
-            //accel --> velocity
+        //note that somehow x and y axes are SWITCHED!
+        $scope.tranA.x = (xyzPercs.y * ($scope.throttle / 100) * 184.16 / 80) + $scope.tranAV.x;
+        $scope.tranA.y = ((xyzPercs.x * ($scope.throttle / 100) * -184.16 / 80) + $scope.grav / 80) + $scope.tranAV.y;
+        $scope.tranA.z = (xyzPercs.z * ($scope.throttle / 100) * -184.16 / 80) + $scope.tranAV.z;
+
+        //accel --> velocity
         $scope.tranV.x += $scope.tranA.x;
         $scope.tranV.y += $scope.tranA.y;
         $scope.tranV.z += $scope.tranA.z;
 
         //velocity to pos
         $scope.tran.x += $scope.tranV.x;
-        // $scope.tran.y+=$scope.tranV.y;
         $scope.tran.z += $scope.tranV.z;
+        if ($scope.activeGame && $scope.activeGame != false) {
+            $scope.tran.y += $scope.tranV.y;
+            // $scope.tran.y += 20;
+        } else {
+            $scope.tran.y = 0;
+        }
         //adjust main engine flame height
         $('.flame').css({ 'height': $scope.throttle + 'px', 'width': (20 * $scope.throttle / 100) + 'px' });
         $scope.$digest();
+        $scope.checkBounds(); //check to see if we're outside the boundaries!
     }, 50)
+    $scope.checkBounds = function() {
+        //out of bounds
+        if ($scope.tran.x > 4000 || $scope.tran.x < -4000 || $scope.tran.z > 4000 || $scope.tran.z < -4000) {
+            clearInterval($scope.t);
+            bootbox.confirm('You\'ve drifted too far from the landing site! Wanna try again?', function(r) {
+                if (r && r != null) {
+                    window.location.reload();
+                }
+            })
+        }
+        if (5000 - $scope.tran.y < -4429) {
+            if ($scope.tranV.y > 15) {
+                clearInterval($scope.t);
+                bootbox.confirm('You\'ve landed too hard, and crashed! Wanna try again?', function(r) {
+                    if (r && r != null) {
+                        window.location.reload();
+                    }
+                })
+            }else{
+                clearInterval($scope.t);
+                bootbox.confirm('Congrats. Ya landed. Wanna try again?', function(r) {
+                    if (r && r != null) {
+                        window.location.reload();
+                    }
+                })
+            }
+        }
+    }
+    $scope.round = function(n) {
+        return Math.floor(n);
+    }
 });
 
 function Vector(x, y, z) {
